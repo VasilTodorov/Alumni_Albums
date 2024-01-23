f8275dc7e13cc40d7e0514159b8e0c9093c3077f
tsankoangelov <63124507+tsankoangelov@users.noreply.github.com>
2024-01-23 20:10:41 +0200

    Add files via upload

---
+++ code/users.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2024 at 07:01 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(99) NOT NULL,
+  `password` varchar(99) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `first  name` varchar(99) NOT NULL,
+  `last name` varchar(99) NOT NULL,
+  `major` varchar(99) NOT NULL,
+  `faculty number` varchar(10) NOT NULL,
+  `graduation` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `first  name`, `last name`, `major`, `faculty number`, `graduation`) VALUES
+(1, 'tsanko84', 'tsanko82084', 'tsanko82084@gmail.com', 'Tsanko', 'Angelov', 'Computer Science', '82084', '2019'),
+(2, 'stefani15', 'stefani82115', 'stefani82115@gmail.com', 'Stefani', 'Stefanova', 'Computer Science', '82115', '2019'),
+(3, 'vasil29', 'vasil82129', 'vasil82129@gmail.com', 'Vasil', 'Todorov', 'Computer Science', '82129', '2020');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `faculty number` (`faculty number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
